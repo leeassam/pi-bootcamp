d08bc087d864206e4d35fd4508263d55a9fe86af
Lee Assam <leeassam@gmail.com>
2018-10-18 14:50:23 -0400

    Made changes for python 3

--- blinkLED.py
+++ blinkLED.py
@@ -8,17 +8,17 @@ GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
 ##Define a function named Blink()
 def Blink(numTimes,speed):
   for i in range(0,numTimes):## Run loop numTimes
-    print "Iteration " + str(i+1)## Print current loop
+    print ("Iteration " + str(i+1)) ## Print current loop
     GPIO.output(7,True)## Switch on pin 7
     time.sleep(speed)## Wait
     GPIO.output(7,False)## Switch off pin 7
     time.sleep(speed)## Wait
-  print "Done" ## When loop is complete, print "Done"
+  print ("Done") ## When loop is complete, print "Done"
   GPIO.cleanup()
 
 ## Ask user for total number of blinks and length of each blink
-iterations = raw_input("Enter total number of times to blink: ")
-speed = raw_input("Enter length of each blink(seconds): ")
+iterations = input("Enter total number of times to blink: ")
+speed = input("Enter length of each blink(seconds): ")
 
 ## Start Blink() function. Convert user input from strings to numeric data types and pass to Blink() as parameters
 Blink(int(iterations),float(speed))
